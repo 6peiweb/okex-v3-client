399abc42fa2c8a52199488a7ee5d908a16e85f72
liupei05 <liupei05@meituan.com>
2019-01-02 17:34:44 +0800

    ok

--- socket.js
+++ socket.js
@@ -30,3 +30,24 @@ ws.on('connection', (socket) => {
   })
   
 })
+
+const path   = require('path')
+const http   = require('http')
+const fs     = require('fs')
+
+const Koa    = require('koa')
+const static = require('koa-static')
+
+const app = new Koa()
+
+const httpsOption = { 
+  key : fs.readFileSync('./ssl/www.liupei.xyz.key'), 
+  cert: fs.readFileSync('./ssl/www.liupei.xyz.pem')
+}
+
+app.use(static(
+  path.join( __dirname,  './public')
+))
+
+http.createServer(app).listen(80);
+https.createServer(httpsOption, app).listen(443);
