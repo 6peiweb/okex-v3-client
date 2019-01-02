2bbc146e83d4265e0ac71bd13d0127ce880a737f
liupei05 <liupei05@meituan.com>
2019-01-02 13:40:20 +0800

    feat: dp accout

--- index.js
+++ index.js
@@ -1,35 +1,35 @@
-const { PublicClient } = require('@okfe/okex-node')
+// const { PublicClient } = require('@okfe/okex-node')
 
-const { getInstrumentId } = require('./src/util')
-const { sendMail } = require('./src/email')
+// const { getInstrumentId } = require('./src/util')
+// const { sendMail } = require('./src/email')
 
-const client = new PublicClient('https://www.okex.me')
+// const client = new PublicClient('https://www.okex.me')
 
-const futures = client.futures()
+// const futures = client.futures()
 
-const getTicker = async () => {
-  const instrumentId = getInstrumentId('BTC').Quarter
-  const response = await futures.getTicker(instrumentId)
-  return response
-}
+// const getTicker = async () => {
+//   const instrumentId = getInstrumentId('BTC').Quarter
+//   const response = await futures.getTicker(instrumentId)
+//   return response
+// }
 
-const WebSocketServer = require('ws').Server
+// const WebSocketServer = require('ws').Server
 
-const ws = new WebSocketServer({port: 80})
+// const ws = new WebSocketServer({port: 80})
 
-ws.on('connection', (socket) => {
-  console.log(`websocket 连接成功...`)
-  socket.on('message', async (msg) => {
-    try {
-      const data = await getTicker()
-      console.log(data)
-      socket.send(JSON.stringify(data))
-    } catch {
-      socket.send('{"last": "timeout"}')
-    }
-  })
+// ws.on('connection', (socket) => {
+//   console.log(`websocket 连接成功...`)
+//   socket.on('message', async (msg) => {
+//     try {
+//       const data = await getTicker()
+//       console.log(data)
+//       socket.send(JSON.stringify(data))
+//     } catch {
+//       socket.send('{"last": "timeout"}')
+//     }
+//   })
   
-})
+// })
 
 const Koa = require('koa')
 const path = require('path')
@@ -41,8 +41,8 @@ app.use(static(
   path.join( __dirname,  './public')
 ))
 
-app.listen(3000, () => {
-  console.log('server is starting at port 3000')
+app.listen(4399, () => {
+  console.log('server is starting at port 4399')
 })
 
 
