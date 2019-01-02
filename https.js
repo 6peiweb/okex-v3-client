6288b0b268404a6fc56de4ceb622f3c88517aafb
liupei05 <liupei05@meituan.com>
2019-01-02 18:18:17 +0800

    ok

--- https.js
+++ https.js
@@ -1,20 +1,27 @@
-const path   = require('path')
 const http   = require('http')
+const https  = require('https')
 const fs     = require('fs')
+const path   = require('path')
 
 const Koa    = require('koa')
 const static = require('koa-static')
+const enforceHttps = require('koa-sslify')
 
 const app = new Koa()
+const PORT = 80
+const SSLPORT = 443
 
-const httpsOption = { 
-  key : fs.readFileSync('./https/www.liupei.xyz.key'), 
+
+app.use(enforceHttps())
+
+app.use(static(path.join( __dirname,  './public')))
+
+const httpServer = http.createServer(app.callback())
+const httpsServer = https.createServer({
+  key: fs.readFileSync('./https/www.liupei.xyz.key'), 
   cert: fs.readFileSync('./https/www.liupei.xyz.pem')
-}
+}, app.callback())
 
-app.use(static(
-  path.join( __dirname,  './public')
-))
+httpServer.listen(PORT, () => console.log('HTTP Server is running on: http://localhost:%s', PORT))
 
-http.createServer(app).listen(80);
-https.createServer(httpsOption, app).listen(443);
+httpsServer.listen(SSLPORT, () => console.log('HTTPS Server is running on: https://localhost:%s', SSLPORT))
--- socket.js
+++ socket.js
@@ -30,24 +30,3 @@ ws.on('connection', (socket) => {
   })
   
 })
-
-const path   = require('path')
-const http   = require('http')
-const fs     = require('fs')
-
-const Koa    = require('koa')
-const static = require('koa-static')
-
-const app = new Koa()
-
-const httpsOption = { 
-  key : fs.readFileSync('./https/www.liupei.xyz.key'), 
-  cert: fs.readFileSync('./https/www.liupei.xyz.pem')
-}
-
-app.use(static(
-  path.join( __dirname,  './public')
-))
-
-http.createServer(app).listen(80);
-https.createServer(httpsOption, app).listen(443);
