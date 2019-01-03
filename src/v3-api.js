ecedfbad0b075111bf8f4fdf973fc79ff58c9d87
liupei05 <liupei05@meituan.com>
2019-01-03 15:59:13 +0800

    ok

--- src/v3-api.js
+++ src/v3-api.js
@@ -1,6 +1,6 @@
 const { AuthenticatedClient } = require('@okfe/okex-node')
 
-const auth = new AuthenticatedClient('f3a5860b-e5f6-4d6e-8477-78690024e47f', 'DE5704A86D042DB3662F79C626629140', 'lp19970127')
+const auth = new AuthenticatedClient('f3a5860b-e5f6-4d6e-8477-78690024e47f', 'DE5704A86D042DB3662F79C626629140', 'lp19970127', 'https://www.okex.me')
 
 const result = async () => {
   const data = await auth.spot().getAccounts()
