87c6a73f86e8083a190c70ffac08472c553ab0d2
liupei05 <liupei05@meituan.com>
2019-01-04 18:32:44 +0800

    ok

--- app.js
+++ app.js
@@ -23,6 +23,7 @@ const ws = new WebSocketServer({port: 8080})
 ws.on('connection', (socket) => {
   console.log(`Websocket connection succeeded...`)
   socket.on('message', async (msg) => {
+    console.log(socket)
     try {
       const data = await getTicker(msg)
       socket.send(JSON.stringify(data))
