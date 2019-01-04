8b55e04418017c3d6f54ac72dcd5ed675cc5458e
liupei05 <liupei05@meituan.com>
2019-01-04 18:30:32 +0800

    ok

--- app.js
+++ app.js
@@ -23,9 +23,8 @@ const ws = new WebSocketServer({port: 8080})
 ws.on('connection', (socket) => {
   console.log(`Websocket connection succeeded...`)
   socket.on('message', async (msg) => {
-    console.log(msg)
     try {
-      const data = await getTicker('BTC')
+      const data = await getTicker(msg)
       socket.send(JSON.stringify(data))
     } catch {
       socket.send('{"last": "timeout"}')
